565463871a9e8eb3bb8cc494ab902317f8adc797
sbendrioue <said.bendrioue3@docaposte.fr>
2025-07-23 12:00:31 +0200

    initial commit spring-boot

---
+++ src/main/java/com/bendrioue/expressmenu/ExpressmenuApplication.java
@@ -0,0 +1,13 @@
+package com.bendrioue.expressmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressmenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bendrioue/expressmenu/ExpressmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bendrioue.expressmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
